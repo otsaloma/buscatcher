41f5c13ab18dcab5a72ea83dcc7d1fd9266daffa
Osmo Salomaa <otsaloma@iki.fi>
2012-12-06 20:09:10 +0200

    Fix miscellaneous issues.
    Sorry for the monster commit. This is a collection of fixes accumulated
    over time. Most importantly this should make buscatcher work, since the
    previously used unofficial KML API seems dead.
    
     * Use the official HSL Live API
     * Only download data from area visible on screen
     * Add direction arrows to vehicle icons
     * Add route numbers to vehicle icons
     * Fix initial setting of location
     * Add support for specifying tile source
    
    For N900, you'll need python-osmgpsmap and libosmgpsmap0 from
    extras-devel.

--- src/__main__.py
+++ src/__main__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """
  Copyright (C) Henri Bergius 2010 <henri.bergius@iki.fi>
+ Copyright (C) Osmo Salomaa 2012 <otsaloma@iki.fi>
  Based on adventure_tablet by:
  Copyright (C) Susanna Huhtanen 2010 <ihmis.suski@gmail.com>
 
@@ -21,8 +22,6 @@
 import gtk, gobject
 import point
 import urllib, urllib2
-from xml.etree import ElementTree as ET
-import tempfile
 import os
 import socket
 import optparse
@@ -55,17 +54,17 @@ except ImportError:
     pass
 
 class buscatcher(gtk.Window):
-    location = None
-    kmlfetch = None
-    buses = {}
-    icons = {}
-    downloads = {}
-    stop_fetching = False
 
-    def __init__(self):
-        win = gtk.Window.__init__(self)
-
-        self.build_ui()
+    def __init__(self, repo_uri, initial_zoom):
+        gtk.Window.__init__(self)
+        self.location = None
+        self.csvfetch = None
+        self.buses = {}
+        self.stop_fetching = False
+        self.initial_zoom = initial_zoom
+        self.times_location_found = 0
+        self.missing_icons = set()
+        self.build_ui(repo_uri)
         self.get_location()
 
         self.icondir = os.path.expanduser('~/.cache/buscatcher')
@@ -75,12 +74,12 @@ class buscatcher(gtk.Window):
         # Set a default timeout for our HTTP requests so they don't hang when cell connection is bad
         socket.setdefaulttimeout(10)
 
-    def build_ui(self):
-        self.set_default_size(500, 500)
+    def build_ui(self, repo_uri):
+        self.set_default_size(800, 480)
         self.connect('destroy', gtk.main_quit, None)
         self.set_title('Helsinki Bus Catcher')
 
-        self.osm = osmgpsmap.GpsMap()
+        self.osm = osmgpsmap.GpsMap(repo_uri=repo_uri)
 
         #connect keyboard shortcuts
         self.osm.set_keyboard_shortcut(osmgpsmap.KEY_FULLSCREEN, gtk.gdk.keyval_from_name("F11"))
@@ -96,36 +95,53 @@ class buscatcher(gtk.Window):
         if busid not in self.buses:
             # First time we see this bus
             self.buses[busid] = bus
-
-        if 'icon' not in self.buses[busid]:
-            # Bus doesn't have an icon yet
-            self.buses[busid]['icon'] = gtk.gdk.pixbuf_new_from_file_at_size(self.icons[bus['styleid']], 55, 55)
         else:
-            self.osm.remove_image(self.buses[busid]['icon'])
-
-        if self.buses[busid]['styleid'] != bus['styleid']:
-            self.buses[busid]['icon'] = gtk.gdk.pixbuf_new_from_file_at_size(self.icons[bus['styleid']], 55, 55)
-
-        self.buses[busid]['location'] = bus['location']
-
-        self.osm.add_image(self.buses[busid]['location'].lat, self.buses[busid]['location'].lon, self.buses[busid]['icon'])
+            self.buses[busid].update(bus)
+        bus = self.buses[busid]
+        try:
+            self.osm.remove_image(bus["type_icon"])
+        except Exception:
+            pass
+        try:
+            self.osm.remove_image(bus["route_icon"])
+        except Exception:
+            pass
+        _pixbuf = gtk.gdk.pixbuf_new_from_file
+        try:
+            bus["type_icon"] = _pixbuf(bus["type_icon_path"])
+            self.osm.add_image(bus["lat"], bus["lon"], bus["type_icon"])
+        except Exception:
+            print "Failed to add image for %s" % repr(busid)
+        try:
+            bus["route_icon"] = _pixbuf(bus["route_icon_path"])
+            self.osm.add_image(bus["lat"], bus["lon"], bus["route_icon"])
+        except Exception:
+            pass
 
     def get_location(self):
-        if options.no_update_position:
-            return
-        elif Geoclue:
+        # if options.no_update_position:
+        #     return
+        if Geoclue:
             self.get_location_geoclue()
         elif location:
-           self.get_location_liblocation()
+            self.get_location_liblocation()
 
     def set_location(self, location):
-        if location.lat == 0.0 or location.lon == 0.0:
-            return
+        # It seems that about the third location data is correct.
+        # (Previous ones are cached or rough values?)
+        if (self.times_location_found > 3 and
+            options.no_update_position): return
+        # Avoid jumps to weird places outside Uusimaa.
+        if not 59.82 < location.lat < 61.08: return
+        if not 22.54 < location.lon < 26.77: return
         self.location = location
-        self.osm.set_mapcenter(self.location.lat, self.location.lon, 15)
+        self.times_location_found += 1
+        self.osm.set_mapcenter(self.location.lat,
+                               self.location.lon,
+                               self.initial_zoom)
 
-        if self.kmlfetch is None:
-            self.kmlfetch = gobject.timeout_add(options.update_interval, self.fetch_kml)
+        if self.csvfetch is None:
+            self.csvfetch = gobject.timeout_add(options.update_interval, self.fetch_csv)
 
     def get_location_liblocation(self):
         self.control = location.GPSDControl.get_default()
@@ -180,8 +196,8 @@ class buscatcher(gtk.Window):
 
     def tracking_start(self):
         self.stop_fetching = False
-        if self.kmlfetch is None:
-            self.kmlfetch = gobject.timeout_add(options.update_interval, self.fetch_kml)
+        if self.csvfetch is None:
+            self.csvfetch = gobject.timeout_add(options.update_interval, self.fetch_csv)
         if Geoclue:
             pass
         elif location:
@@ -194,89 +210,131 @@ class buscatcher(gtk.Window):
         elif location:
             self.control.stop()
 
-    def fetch_kml(self):
+    def build_csv_url(self):
+        # See HSL Live 1.6 documentation.
+        # http://developer.reittiopas.fi/pages/en/other-apis.php
+        bbox = self.osm.get_bbox()
+        lat = osmgpsmap.point_new_radians(bbox[0], bbox[2]).get_degrees()
+        lon = osmgpsmap.point_new_radians(bbox[1], bbox[3]).get_degrees()
+        return ("http://83.145.232.209:10001/?type=vehicles"
+                "&lng1=%.6f&lat1=%.6f&lng2=%.6f&lat2=%.6f" %
+                (min(lon), min(lat), max(lon), max(lat)))
+
+    def fetch_csv(self):
         if self.stop_fetching:
-            self.kmlfetch = None
+            self.csvfetch = None
             return False
 
+        url = self.build_csv_url()
         opener = urllib2.build_opener()
         opener.addheaders = [('User-agent', 'buscatcher/0.1')]
         try:
-            req = opener.open(options.kml_url)
-            kml = req.read(100000)
+            req = opener.open(url)
+            csv = req.read(100000)
         except urllib2.HTTPError, e:
-            print('KML HTTP error %s' % (e.code))
+            print('CSV HTTP error %s' % (e.code))
             return True
         except urllib2.URLError, e:
-            print("KML Connection failed, error %s" % (e.message))
+            print("CSV Connection failed, error %s" % (e.message))
             return True
         except IOError, e:
-            print "KML Connection failed"
+            print "CSV Connection failed"
             return True
 
-        self.parse_kml(kml)
+        self.parse_csv(csv)
 
         return True
 
-    def parse_kml(self, kmlxml):
-        kml = ET.fromstring(kmlxml)
-        buses = []
-        for document in kml:
-            for folder in document:
-                if folder.tag == '{http://www.opengis.net/kml/2.2}Folder':
-                    for placemark in folder:
-                        if placemark.tag != '{http://www.opengis.net/kml/2.2}Placemark':
-                            continue
-                        
-                        bus = {}
-                        bus['id'] = placemark.get('id')
-                        numberelement = placemark.find('{http://www.opengis.net/kml/2.2}name')
-                        bus['number'] = numberelement.text
-                        pointelement = placemark.find('{http://www.opengis.net/kml/2.2}Point').find('{http://www.opengis.net/kml/2.2}coordinates')
-                        locationstring = pointelement.text.split(',')
-                        bus['location'] = point.point(locationstring[1], locationstring[0])
-                        bus['styleid'] = placemark.find('{http://www.opengis.net/kml/2.2}styleUrl').text[1:]
-                        buses.append(bus)
+    def parse_csv(self, csv):
+        # See HSL Live 1.6 documentation.
+        # http://developer.reittiopas.fi/pages/en/other-apis.php
+        csv = csv.splitlines()
+        for line in csv:
+            items = line.split(";")
+            try:
+                bus = {"id": items[0],
+                       "route": items[1],
+                       "lon": float(items[2]),
+                       "lat": float(items[3]),
+                       "bearing": float(items[4])}
 
-                elif folder.tag == '{http://www.opengis.net/kml/2.2}Style':
-                    style = folder
-                    styleid = style.get('id')
-                    iconurl = style.find('{http://www.opengis.net/kml/2.2}IconStyle').find('{http://www.opengis.net/kml/2.2}Icon').find('{http://www.opengis.net/kml/2.2}href').text
-                    iconname = os.path.basename(iconurl)
-                    iconpath = self.icondir + '/' + iconname
-
-                    if iconurl not in self.downloads:
-                        if not os.path.exists(iconpath):
-                            print "Downloading " + iconurl
-                            web = urllib.urlopen(iconurl)
-                            local = open(iconpath, 'w')
-                            local.write(web.read(10000))
-                            web.close()
-                            local.close()
-                        self.downloads[iconurl] = iconpath
-
-                    self.icons[styleid] = iconpath
-
-        for bus in buses:
+            except Exception:
+                continue
+            icons = self.download_icons(bus)
+            bus["type_icon_path"] = icons[0]
+            bus["route_icon_path"] = icons[1]
             self.update_bus(bus)
+
+    def download_icons(self, bus):
+        # Let's borrow icons from the official web interface.
+        # http://transport.wspgroup.fi/hklkartta/
+        if bus["id"].startswith("RHKL"):
+            type_name = "tram"
+        elif bus["id"].startswith("metro"):
+            type_name = "metro"
+        else:
+            # TODO: Add trains.
+            type_name = "bus"
+        bearing = round(bus["bearing"]/45.0)*45
+        if bearing > 315: bearing = 0
+        type_url = ("http://transport.wspgroup.fi/"
+                    "hklkartta/images/vehicles/%s%.0f.png"
+                    % (type_name, bearing))
+
+        route_url = ("http://transport.wspgroup.fi/"
+                     "hklkartta/images/vehicles/%s.png"
+                     % bus["route"])
+
+        type_basename = type_url.split("/")[-1]
+        route_basename = route_url.split("/")[-1]
+        type_path = os.path.join(self.icondir, type_basename)
+        route_path = os.path.join(self.icondir, route_basename)
+        if (not type_basename in self.missing_icons and
+            not os.path.exists(type_path)):
+            print "Downloading " + type_url
+            web = urllib.urlopen(type_url)
+            # Avoid downloading 404-page HTML.
+            if web.info().gettype() == "image/png":
+                local = open(type_path, 'w')
+                local.write(web.read(10000))
+                local.close()
+            else:
+                # Avoid trying the same icon again.
+                self.missing_icons.add(type_basename)
+            web.close()
+        if (not route_basename in self.missing_icons and
+            not os.path.exists(route_path)):
+            print "Downloading " + route_url
+            web = urllib.urlopen(route_url)
+            # Avoid downloading 404-page HTML.
+            if web.info().gettype() == "image/png":
+                local = open(route_path, 'w')
+                local.write(web.read(10000))
+                local.close()
+            else:
+                # Avoid trying the same icon again.
+                self.missing_icons.add(route_basename)
+            web.close()
+        return (type_path, route_path)
+
 
 if __name__ == "__main__":
     parser = optparse.OptionParser("buscatcher OPTIONS")
-    parser.add_option("--kml-url", type="string", default="http://hkl.seuranta.org/kml")
-    parser.add_option("--update-interval", type="int", default=5000)
-    parser.add_option("--initial-lat", type="float", default=0.0)
-    parser.add_option("--initial-lon", type="float", default=0.0)
-    parser.add_option("--no-update-position", action="store_true")
+    parser.add_option("--update-interval", type="int", default=3000)
+    parser.add_option("--initial-lat", type="float", default=60.170424)
+    parser.add_option("--initial-lon", type="float", default=24.94070)
+    parser.add_option("--initial-zoom", type="int", default=15)
+    parser.add_option("--repo-uri", type="str",default="http://tile.openstreetmap.org/#Z/#X/#Y.png")
+    parser.add_option("--no-update-position", action="store_true", default=True)
     (options, args) = parser.parse_args()
 
     if conic:
         # Request Maemo to start an internet connection, as buscatcher doesn't really make sense without one
         connection = conic.Connection()
 
-    u = buscatcher()
-    if options.initial_lat != 0.0:
-        initial_location = point.point(options.initial_lat, options.initial_lon)
-        u.set_location(initial_location)
+    u = buscatcher(options.repo_uri, options.initial_zoom)
+    initial_location = point.point(options.initial_lat, options.initial_lon)
+    u.set_location(initial_location)
     u.show_all()
 
     if osso:
